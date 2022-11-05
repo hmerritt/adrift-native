d753401b1e032cd9fb1cdd2a0c9ce1d6481715ad
hmerritt <harry@mrrtt.me>
2022-11-05 15:42:06 +0000

    wip: port default template to hmerritt/cra

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,38 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+// @ts-nocheck
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import linaria from "./config/linaria-rollup.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	build: {
+		sourcemap: false,
+		minify: true
+	},
+	define: {
+		"process.env": {}
+	},
+	plugins: [
+		react({
+			// jsxRuntime: "classic",
+			babelrc: true,
+			configFile: true
+		}),
+		tsconfigPaths(),
+		linaria({
+			sourceMap: false,
+			extension: ".scss",
+			preprocessor: "none"
+		})
+	],
+	test: {
+		// https://vitest.dev/api/
+		globals: false,
+		environment: "happy-dom",
+		setupFiles: "./src/tests/setupTests.ts",
+		// Parsing CSS is slow
+		css: false
+	}
+});
