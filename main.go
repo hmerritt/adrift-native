7e409f97d59b93697d54d23134319a7361c1fda1
hmerritt <harry@mrrtt.me>
2023-06-21 14:45:08 +0100

    feat: update default wails app options

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
 
 func main() {
 	// Create an instance of the app structure
@@ -16,14 +22,43 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "go-gui",
-		Width:  1024,
-		Height: 768,
-		// Frameless: true,
+		Title:             "go-gui",
+		Width:             1024,
+		Height:            768,
+		MinWidth:          300,
+		MinHeight:         300,
+		DisableResize:     false,
+		Fullscreen:        false,
+		Frameless:         false,
+		StartHidden:       false,
+		HideWindowOnClose: true,
+		// RGBA:              &options.RGBA{255, 255, 255, 255},
 		Assets:    assets,
 		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		// Windows platform specific options
+		Windows: &windows.Options{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			DisableWindowIcon:    false,
+			IsZoomControlEnabled: true,
+			ZoomFactor:           1,
+		},
+		Mac: &mac.Options{
+			TitleBar:             mac.TitleBarHiddenInset(),
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			About: &mac.AboutInfo{
+				Title:   "Adrift Native Template",
+				Message: "Adrift Native via Wails",
+				Icon:    icon,
+			},
+		},
+		Linux: &linux.Options{
+			Icon:                icon,
+			WindowIsTranslucent: false,
 		},
 	})
 
